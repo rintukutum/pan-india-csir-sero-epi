462a89683aeb35db3052c4f66dbb1149e59c2e88
rintukutum <rintukutum@gmail.com>
2021-04-23 00:45:54 +0530

    updated the latest code corresponding the published manuscript

--- fig04.R
+++ fig04.R
@@ -1,21 +1,24 @@
 rm(list=ls())
+dir.create('./figures/joint/200321', showWarnings = FALSE)
 roche.data <- read.csv(
-  './data/231220/antibody-3-months-251220.csv',
+  './data/2021-03-20/roche-data.csv',
   stringsAsFactors = FALSE
 )
+# 607
 colnames(roche.data)[2:3] <- c('baseline','month3')
-
+#roche.data$baseline[roche.data$baseline < 0] <- 0
+#roche.data$month3[roche.data$month3 < 0] <- 0
 roche.data$delta <- roche.data$month3 - roche.data$baseline
 library(ggplot2)
 library(ggpubr)
-pdf('./figures/joint/261220/Figure-04-A.pdf',width = 5,height = 5)
+pdf('./figures/joint/200321/Figure-04-A-200321.pdf',width = 5,height = 5)
 ggplot(roche.data,
        aes(x=log2(baseline),y=log2(month3))) +
   geom_abline(slope = 1,linetype='dashed') +
-  annotate("rect", xmin = -4, xmax = 0, ymin = -4, ymax = 8,
-           alpha = 0.08,fill='#c837abff') + 
-  annotate("rect", ymin = -4, ymax = 0, xmin = -4, xmax = 8,
-           alpha = 0.08,fill='#0055d4ff') + 
+  annotate("rect", xmin = -4, xmax = 0, ymin = -4, ymax = 9,
+           alpha = 0.08,fill='#c837abff') +
+  annotate("rect", ymin = -4, ymax = 0, xmin = -4, xmax = 9,
+           alpha = 0.08,fill='#0055d4ff') +
   geom_point(aes(size=abs(delta),fill=delta),shape=21,) +
   scale_fill_gradient2(
     name = "",
@@ -23,14 +26,14 @@ ggplot(roche.data,
     low = '#0055d4ff',
     mid = 'white',
     high = '#c837abff') +
-  
+
   scale_y_continuous(
-    limits = c(-4,8),
+    limits = c(-4,9),
     breaks = c(-4,-2,0,2,4,6,8),
     labels = c(-4,-2,0,2,4,6,8)
   ) +
   scale_x_continuous(
-    limits = c(-4,8),
+    limits = c(-4,9),
     breaks = c(-4,-2,0,2,4,6,8),
     labels = c(-4,-2,0,2,4,6,8)) +
   scale_size(name= "") +
@@ -41,23 +44,25 @@ dev.off()
 
 
 inhibition.data <- read.csv(
-  './data/231220/antibody-neutra-251220.csv',
+  './data/2021-03-20/antibody-neutra.csv',
   stringsAsFactors = FALSE
 )
+# 607
 colnames(inhibition.data)[2:3] <- c('baseline','month3')
 
 inhibition.data$month3[inhibition.data$month3 < 0] <- 0
-
+# [1] -4.934597
+# inhibition.data$baseline[inhibition.data$baseline < 0] <- 0
 inhibition.data$delta <- inhibition.data$month3 - inhibition.data$baseline
 
 
-pdf('./figures/joint/261220/Figure-04-B.pdf',width = 5,height = 5)
+pdf('./figures/joint/200321/Figure-04-B-200221.pdf',width = 5,height = 5)
 ggplot(inhibition.data,
        aes(x=baseline,y=month3)) +
   annotate("rect", xmin = 0, xmax = 20, ymin = 0, ymax = 100,
-           alpha = 0.08,fill='#2ca089ff') + 
+           alpha = 0.08,fill='#2ca089ff') +
   annotate("rect", xmin = 0, xmax = 100, ymin = 0, ymax = 20,
-           alpha = 0.08,fill='#aa0000ff') + 
+           alpha = 0.08,fill='#aa0000ff') +
   geom_abline(intercept = 0, slope = 1,
               linetype='dashed') +
   geom_point(aes(size=abs(delta),fill=delta),shape=21) +
@@ -69,87 +74,87 @@ ggplot(inhibition.data,
     low = '#aa0000ff',
     mid = 'white',
     high = '#2ca089ff') +
-  #scale_size(name= "absolute of \u0394") +
   scale_size(name= "") +
   xlab('Neutralising antibody (%) [baseline]') +
   ylab('Neutralising antibody (%) [3 months]') +
   theme_pubr()
 dev.off()
 #-----------------
-# 6 months data
+# 5/6 months data
 rm(list=ls())
-dyn.data <- read.csv(
-  './data/221220/6-months-data.csv',
+roche.data <- read.csv(
+  './data/2021-03-20/roche-data-5-6-months-175-followup.csv',
   stringsAsFactors = FALSE
 )
-getThresholdCode <- function(x,var=c(2,3)){
-  x.var <- x[,var]
-  names(x.var) <- x$time
-  j <- 1
-  out <- c()
-  for(i in 2:length(x.var)){
-    x10 <- (x.var[i-1]*5)/100
-    neutral <- TRUE
-    if((x.var[i-1] + x10) < x.var[i]){
-      out[j] <- 'Increase'
-      neutral <- FALSE
-    }
-    if((x.var[i-1] - x10) > x.var[i]){
-      out[j] <- 'Decrease'
-      neutral <- FALSE
-    }
-    if(neutral){
-      out[j] <- 'Same\n(+/-5%)'
-    }
-    j <- j + 1
-  }
-  
-  if(nrow(x) == 3){
-    df_ <- rbind(x,x[2,])
-    df_$path <- paste0(unique(df_$Sample.IDs),':','P',rep(c(1:2),each=2))
-    df_$trend <- rep(out,each=2)
-  }else{
-    df_ <- x
-    df_$path <- paste0(unique(df_$Sample.IDs),':','P1')
-    df_$trend <- rep(out,each=2)
-  }
-  return(df_)
-}
-dyn.data.ab <- plyr::ddply(dyn.data,'Sample.IDs',function(x){getThresholdCode(x,2)})
-dyn.data.nab <- plyr::ddply(dyn.data,'Sample.IDs',function(x){getThresholdCode(x,3)})
-
+# 175
+colnames(roche.data)[2:3] <- c('baseline','month6')
+roche.data$delta <- roche.data$month6 - roche.data$baseline
 library(ggplot2)
 library(ggpubr)
+dir.create('./figures/joint/200321',showWarnings = FALSE)
+pdf('./figures/joint/200321//Figure-04-C-6months-175.pdf',width = 5,height = 5)
+ggplot(roche.data,
+       aes(x=log2(baseline),y=log2(month6))) +
+  geom_abline(slope = 1,linetype='dashed') +
+  annotate("rect", xmin = -4, xmax = 0, ymin = -4, ymax = 9,
+           alpha = 0.08,fill='#c837abff') +
+  annotate("rect", ymin = -4, ymax = 0, xmin = -4, xmax = 9,
+           alpha = 0.08,fill='#0055d4ff') +
+  geom_point(aes(size=abs(delta),fill=delta),shape=21,) +
+  scale_fill_gradient2(
+    name = "",
+    midpoint = 0,
+    low = '#0055d4ff',
+    mid = 'white',
+    high = '#c837abff') +
 
-p.ab <- ggplot(
-  dyn.data.ab,
-  aes(x=Roche.values,y=time,group=path)
-) +
-  geom_line(aes(col=trend),size=0.8) +
-  geom_point(shape=21,col='black',fill='grey95',size=4) +
-  coord_flip() +
-  scale_y_discrete(limit=c('baseline','3 months','6 months'),
-                   label = c('Baseline',' 3 Months','6 Months')) +
-  scale_color_discrete(name='') +
-  theme_pubr() +
-  xlab('Antibody') +
-  ylab('')
+  scale_y_continuous(
+    limits = c(-4,9),
+    breaks = c(-4,-2,0,2,4,6,8),
+    labels = c(-4,-2,0,2,4,6,8)
+  ) +
+  scale_x_continuous(
+    limits = c(-4,9),
+    breaks = c(-4,-2,0,2,4,6,8),
+    labels = c(-4,-2,0,2,4,6,8)) +
+  scale_size(name= "") +
+  xlab('log2(Antibody) [baseline]') +
+  ylab('log2(Antibody) [5/6 months]') +
+  theme_pubr()
+dev.off()
 
-p.nab <- ggplot(
-  dyn.data.nab,
-  aes(x=Inhibition.,y=time,group=path)
-) +
-  geom_line(aes(col=trend),size=0.8) +
-  geom_point(shape=21,col='black',fill='grey95',size=4) +
-  coord_flip() +
-  scale_y_discrete(limit=c('baseline','3 months','6 months'),
-                   label = c('Baseline',' 3 Months','6 Months')) +
-  scale_color_discrete(name='') +
-  theme_pubr() +
-  xlab('Neutralizing antibody') +
-  ylab('')
+inhibition.data <- read.csv(
+  './data/2021-03-20/antibody-neutra-5-6-months-175-followup.csv',
+  stringsAsFactors = FALSE
+)
+# 175
+colnames(inhibition.data)[2:3] <- c('baseline','month6')
 
-library(gridExtra)
-pdf('./figures/joint/261220/Figure-04-CD.pdf',width = 10,height = 4.5)
-gridExtra::grid.arrange(p.ab,p.nab,nrow=1)
+inhibition.data$month6[inhibition.data$month6 < 0] <- 0
+inhibition.data$baseline[inhibition.data$baseline < 0] <- 0
+inhibition.data$delta <- inhibition.data$month6 - inhibition.data$baseline
+
+
+pdf('./figures/joint/200321/Figure-04-D-6months-175.pdf',width = 5,height = 5)
+ggplot(inhibition.data,
+       aes(x=baseline,y=month6)) +
+  annotate("rect", xmin = 0, xmax = 20, ymin = 0, ymax = 100,
+           alpha = 0.08,fill='#2ca089ff') +
+  annotate("rect", xmin = 0, xmax = 100, ymin = 0, ymax = 20,
+           alpha = 0.08,fill='#aa0000ff') +
+  geom_abline(intercept = 0, slope = 1,
+              linetype='dashed') +
+  geom_point(aes(size=abs(delta),fill=delta),shape=21) +
+  geom_abline(slope = 1,linetype='dashed') +
+  scale_fill_gradient2(
+    #name = "\u0394 \n(3 months - baseline)",
+    name = "",
+    midpoint = 0,
+    low = '#aa0000ff',
+    mid = 'white',
+    high = '#2ca089ff') +
+  scale_size(name= "") +
+  xlab('Neutralising antibody (%) [baseline]') +
+  ylab('Neutralising antibody (%) [5/6 months]') +
+  theme_pubr()
 dev.off()
